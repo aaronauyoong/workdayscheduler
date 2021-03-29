cffaae1f25885c71817d4fbee043ec5d92cd5549
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-03-29 19:01:15 +0800

    cfixed function to updateTimeblocks, cleaned code, removed console.logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,5 @@
 // Define main variables
-let timeNow = moment();
-let hourNow = moment().hour();
 let eventSaveBtn = $(".saveBtn");
-
-// console.log(hourJSON);
-
-// Hourly variables
-// let time9 = $("#9am").text();
-// let time10 = $("#10am").text();
-// let time11 = $("#11am").text();
-// let time12 = $("#12pm").text();
-// let time13 = $("#1pm").text();
-// let time14 = $("#2pm").text();
-// let time15 = $("#3pm").text();
-// let time16 = $("#4pm").text();
-// let time17 = $("#5pm").text();
-// console.log(time9);
-// console.log(time11);
-
-// Array to store each hour (extracted from html timeblock id)
-let $arrayTime = [];
-$('textarea').each(function(){
-    let id = $(this).attr('id');
-    $arrayTime.push(id);
-});
-
-let arrayExtract = $arrayTime[1]
-console.log(arrayExtract)
-console.log($arrayTime);
 
 // Display current time by using moment.js, and updating time every second using jQuery
 let updateTime = function () {
@@ -39,95 +11,30 @@ $(document).ready(function () {
     datetime = $('#currentDay')
     updateTime();
     setInterval(updateTime, 1_000);
+
+    updateTimeblocks();
+    // updates every 15 minutes
+    setInterval(updateTimeblocks, 900_000);
 });
 
-// For loop to set timeblock colour
 
-// for (let index = 0; index <= $arrayTime.length; index++) {
-//     if (hourNow === $arrayTime[index]) {
-//         $("textarea").addClass("present")
-//     } else if (hourNow > $arrayTime[index]) {
-//         $("textarea").addClass("future").removeClass("present")
-//     } else {
-//         $("textarea").addClass("past").removeClass("future present")
-//     };
+function updateTimeblocks() {
+    let hourNow = moment().hour();
 
-// };
+    $(".time-block").each(function () {
+        let timeBlockHour = parseInt($(this).attr("id"));
 
-// function updateTimeblock() {
-//     // current hour = hourNow
+        $(this).find("textarea").removeClass("present past future");
 
-//     // Loop over time blocks
-//     $(".time-block").each(function () {
-//         let timeBlockHour = parseInt($(this).attr("id"));
-
-//         console.log($(this).find("textarea")[0]);
-
-//         if (hourNow === timeBlockHour) {
-//             $(this).find("textarea")[0].element.classList.add("present")
-//         } else if (hourNow > timeBlockHour) {
-//             $(this).find("textarea")[0].element.classList.add("future")
-//         } else {
-//             $(this).find("textarea")[0].element.classList.add("past")
-//         };
-//     });
-
-// }
-
-// updateTimeblock();
-
-// Function for time block colour - colour coded to represent past, present or future
-// function setTimeblockColor() {
-
-//     for (let index = 9; index <= $arrayTime; index++) {
-    
-//         if (hourJSON === $arrayTime[index]) {
-//             $("#9").addClass("present")
-//         } else if (hourJSON > $arrayTime[index]) {
-//             $("#9").addClass("future")
-//         } else {
-//             $("#9").addClass("past")
-//         };
-        
-//     };
-
-    // hourJSON === time9 ? $("#9am").addClass("present")
-    // : hourJSON > time9 ? $("#9am").addClass("future")
-    // : $("#9am").addClass("past");
-
-    // hourJSON === time10 ? $(".hour").addClass("present")
-    // : hourJSON > time10 ? $(".hour").addClass("future")
-    // : $(".hour").addClass("past");
-
-    // hourJSON === time11 ? $("textarea").addClass("present")
-    // : hourJSON > time11 ? $("textarea").addClass("future")
-    // : $("textarea").addClass("past");
-
-    // hourJSON === time12 ? $("textarea").addClass("present")
-    // : hourJSON > time12 ? $("textarea").addClass("future")
-    // : $("textarea").addClass("past");
-
-    // hourJSON === time13 ? $("textarea").addClass("present")
-    // : hourJSON > time13 ? $("textarea").addClass("future")
-    // : $("textarea").addClass("past");
-
-    // hourJSON === time14 ? $("textarea").addClass("present")
-    // : hourJSON > time14 ? $("textarea").addClass("future")
-    // : $("textarea").addClass("past");
-
-    // hourJSON === time15 ? $("textarea").addClass("present")
-    // : hourJSON > time15 ? $("textarea").addClass("future")
-    // : $("textarea").addClass("past");
-
-    // hourJSON === time16 ? $("textarea").addClass("present")
-    // : hourJSON > time16 ? $("textarea").addClass("future")
-    // : $("textarea").addClass("past");
-
-    // hourJSON === time17 ? $("textarea").addClass("present")
-    // : hourJSON > time17 ? $("textarea").addClass("future")
-    // : $("textarea").addClass("past");
-
-// };
+        if (hourNow === timeBlockHour) {
+            $(this).find("textarea").addClass("present")
+        } else if (hourNow > timeBlockHour) {
+            $(this).find("textarea").addClass("past")
+        } else {
+            $(this).find("textarea").addClass("future")
+        };
+    })
+};
 
 // Save button will allow user's plans to be saved to localStorage
 eventSaveBtn.on('click', function() {
@@ -149,19 +56,4 @@ function startDayScheduler() {
     });
 };
 
-// Replace text in divs with hour class to AM/PM 
-// function replaceHours() {
-//     $("#9am").replaceWith('<div class="hour col-2"> 9AM </div>');
-//     $("#10am").replaceWith('<div class="hour col-2"> 10AM </div>');
-//     $("#11am").replaceWith('<div class="hour col-2"> 11AM </div>');
-//     $("#12pm").replaceWith('<div class="hour col-2"> 12PM </div>');
-//     $("#1pm").replaceWith('<div class="hour col-2"> 1PM </div>');
-//     $("#2pm").replaceWith('<div class="hour col-2"> 2PM </div>');
-//     $("#3pm").replaceWith('<div class="hour col-2"> 3PM </div>');
-//     $("#4pm").replaceWith('<div class="hour col-2"> 4PM </div>');
-//     $("#5pm").replaceWith('<div class="hour col-2"> 5PM </div>');
-// };
-
-// setTimeblockColor();
-// replaceHours();
 startDayScheduler();
